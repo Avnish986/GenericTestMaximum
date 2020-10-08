49bf9424f16a406ca126f9f96f98df48eba4a188
avnish986 <avnishgupta986@gmail.com>
2020-10-08 23:26:21 +0530

    [Avnish]Added: among 3 float return greatest using 3 tc

--- src/main/java/com/cg/test/max/TestMaximum.java
+++ src/main/java/com/cg/test/max/TestMaximum.java
@@ -12,6 +12,16 @@ public class TestMaximum {
 		}
 		return max;
 	}
+	public float maximumOfThree(Float a, Float b, Float c) {
+		Float max = a;
+		if(b.compareTo(max)>0) {
+			max=b;
+		}
+		if(c.compareTo(max)>0) {
+			max=c;
+		}
+		return max;
+	}
 	public static void main(String[] args) {
 		System.out.println("Test Maximum");
 
