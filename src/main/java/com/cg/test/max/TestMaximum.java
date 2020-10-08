b4c510c1ccc942395561630e70581a27803d8092
avnish986 <avnishgupta986@gmail.com>
2020-10-08 23:24:16 +0530

    [Avnish]Added: among 3 integer return greatest using 3 tc

--- src/main/java/com/cg/test/max/TestMaximum.java
+++ src/main/java/com/cg/test/max/TestMaximum.java
@@ -1,7 +1,17 @@
 package com.cg.test.max;
 
 public class TestMaximum {
-
+	
+	public int maximumOfThree(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if(b.compareTo(max)>0) {
+			max=b;
+		}
+		if(c.compareTo(max)>0) {
+			max=c;
+		}
+		return max;
+	}
 	public static void main(String[] args) {
 		System.out.println("Test Maximum");
 
